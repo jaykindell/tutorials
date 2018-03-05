4e830d104f2f613b835afe8fd4ddca31cd3c3ea5
Jay Kindell <jaykindell@gmail.com>
2018-03-05 15:56:22 -0600

    (init) adds chat app and index.html

---
+++ nexmo/index.js
@@ -0,0 +1,5 @@
+// Application created: 17c58a40-97ed-4710-8c68-59e5596723cb
+// Conversation created : CON-e20efd94-a6f4-4842-ba26-f05f7ff8314c
+
+// Jay - User created: USR-353c5295-dd68-499a-bef3-4f4084badf6a
+
